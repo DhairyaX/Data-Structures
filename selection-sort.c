abc301850467118df1aa0f96d3186d9284029cdb
Dhairya Rohilla <143923235+DhairyaX@users.noreply.github.com>
2024-04-16 14:31:56 +0530

    Update selection-sort.c

--- selection-sort.c
+++ selection-sort.c
@@ -4,14 +4,14 @@ void selection(int arr[], int n)
 {  
     int i, j, small;  
       
-    for (i = 0; i < n-1; i++)    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n-1; i++)      
     {  
         small = i; //minimum element in unsorted array  
           
         for (j = i+1; j < n; j++)  
         if (arr[j] < arr[small])  
             small = j;  
-// Swap the minimum element with the first element  
+ 
     int temp = arr[small];  
     arr[small] = arr[i];  
     arr[i] = temp;  
